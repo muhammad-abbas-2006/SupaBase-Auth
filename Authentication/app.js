a87c880968d951e270952e5478f71c9cac7ecaf3
Muhammad Abbas <muhammadabuabbas907@gmail.com>
2026-03-07 01:09:14 +0500

    Update app.js

--- Authentication/app.js
+++ Authentication/app.js
@@ -1,7 +1,7 @@
 // Working for signup page
 
-const supabaseUrl = 'https://grdpokfywpqvporuxfry.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdyZHBva2Z5d3BxdnBvcnV4ZnJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQzODc3NTEsImV4cCI6MjA2OTk2Mzc1MX0.vMX2To9Sa3ltrlnyOOZ_DGAO-tMhVumX08qiEQ2k6P8'
+const supabaseUrl = 
+const supabaseKey =
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey)
 
 
@@ -27,4 +27,5 @@ else{
 window.open('./login.html','_self')
 } 
 })
+
 
